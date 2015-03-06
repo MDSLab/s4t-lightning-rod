5675dff7a38bf75048d4aa324418e6d6153996a4
Andrea Rocco Lotronto <andrealotronto@gmail.com>
2015-03-06 12:35:37 +0100

    creaded a new sperimental client for raspberry

--- s4t-wamp-client_rasp.js
+++ s4t-wamp-client_rasp.js
@@ -18,18 +18,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 var wts = require("node-reverse-wstunnel");
-
 var autobahn = require('autobahn');
+
 var os = require('os');
 var ifaces = os.networkInterfaces();
 
-var url_wamp_router = "ws://172.17.3.139:8181/ws";
+var url_wamp_router = "ws://212.189.207.109:8181/ws";
 var url_reverse_server = "ws://212.189.207.109:8080";
 
 var connection = new autobahn.Connection({
 	url: url_wamp_router,
 	realm: "s4t"
 });
+
+//var linino = require('ideino-linino-lib');
+//var board = new linino.Board();
+
+var gpio = require("pi-gpio");
 
 var topic_command = 'board.command';
 var topic_connection = 'board.connection';
@@ -39,6 +44,19 @@ var IPLocal = '127.0.0.1';//getIP('eth0', 'IPv4');
 
 
 connection.onopen = function (session, details) {
+
+   //Define a RPC to Read Data from PIN
+   function readDigital(args){
+      if (args[0]==os.hostname()){
+         value = gpio.readSync(args[2]);
+         return value;
+      }
+   }
+   
+
+   //Register a RPC for remoting
+   session.register(os.hostname()+'.command.rpc.read.digital', readDigital);
+   //session.register(os.hostname()+'.command.rpc.read.analog', readAnalog);
 
    // Publish, Subscribe, Call and Register
    console.log("Connected to WAMP router: "+url_wamp_router);
@@ -50,71 +68,57 @@ connection.onopen = function (session, details) {
 
    //Gestione chiusura comunicazione al server
    process.on('SIGINT', function(){
-   	session.publish(topic_connection, [os.hostname(), 'disconnect']);
-   	process.exit();
+      session.publish(topic_connection, [os.hostname(), 'disconnect']);
+      process.exit();
    });
 
-	//Gestisco topic per i comandi
+   //Gestisco topic per i comandi
    var onCommandMessage = function (args){
-   	console.log('Receive:::'+args[0]);
-      console.log('')
+      console.log('Receive:::'+args[0]);
 
-   	if(args[0]==os.hostname()){
-   		switch(args[1]){
-   			case 'ssh':
+      if(args[0]==os.hostname()){
+         switch(args[1]){
+            case 'ssh':
                //DEBUG
                console.log("Start REVERSE for ssh");
-   				
-   				var reverse_client_ssh = new wts.client_reverse;
-   				
+               
+               var reverse_client_ssh = new wts.client_reverse;
+               
                //DEBUG
                console.log(typeof(reverse_client_ssh));
                console.log(args[2]+','+url_reverse_server+','+IPLocal+':22')
                
                reverse_client_ssh.start(args[2], url_reverse_server, IPLocal+':22');
-   				break;
-   			case 'ideino':
-   				//DEBUG
-               console.log("Start REVERSE for ideino");
-               
-   				var reverse_client_ideino = new wts.client_reverse;
-   				//DEBUG
-               console.log(typeof(reverse_client_ideino));
-               console.log(args[2]+','+url_reverse_server+','+IPLocal+':2424')
-               
-               reverse_client_ideino.start(args[2], url_reverse_server, IPLocal+':2424');
-   				break;
-            case 'osjs':
-               //DEBUG
-               console.log("Start REVERSE for osjs");               
-               var reverse_client_ideino = new wts.client_reverse;
-
-               //DEBUG
-               console.log(typeof(reverse_client_ideino));
-               console.log(args[2]+','+url_reverse_server+','+IPLocal+':')
-               
-               reverse_client_ideino.start(args[2], url_reverse_server, IPLocal+':');
                break;
 
             case 'mode':
+               //DEBUG Message
+               console.log('Set PIN MODE');
+               console.log('message::::'+args);
+               gpio.open(args[2]);
+               //board.pinMode(args[2],args[3]);
+               gpio.setDirection(args[2],args[3]);
                break;
-            case 'analog':
-               break;
+           
             case 'digital':
-               break;
-   		}
-   	}
+               
+                if(args[3]!= undefined){
+                  //DEBUG Message
+                  console.log('DIGITAL WRITE');
+                  console.log('message::::'+args);
+                  //board.digitalWrite(args[2],parseInt(args[3]));
+                  gpio.write(args[2],parseInt(args[3]));
+                  break;
+                }
+         }
+      }
    }
 
    session.subscribe(topic_command, onCommandMessage);
 };
 
-
 connection.onclose = function (reason, details) {
-   // handle connection lost
-   
+   // handle connection lost  
 }
-
-
 
 connection.open();
