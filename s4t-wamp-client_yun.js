8afb7107a8403344ab13720fcc4d624ff596060a
Andrea Rocco Lotronto <andrealotronto@gmail.com>
2015-06-29 12:43:19 +0200

    inserted measure function for ckan smartME

--- s4t-wamp-client_yun.js
+++ s4t-wamp-client_yun.js
@@ -9,6 +9,8 @@ var wts = require("node-reverse-wstunnel");
 var autobahn = require('autobahn');
 var spawn = require('child_process').spawn;
 
+var http = require('http');
+
 var os = require('os');
 var ifaces = os.networkInterfaces();
 
@@ -42,12 +44,14 @@ var topic_connection = 'board.connection';
 var getIP = require('./lib/getIP.js'); //in this moment is not used
 var IPLocal = '127.0.0.1';//getIP('eth0', 'IPv4');
 
-//----------Arthur New-------------------
 var socatClient = [];
 var rtClient = [];
-//---------------------------------------
-
 var servicesProcess=[];
+var misuraTemp;
+
+//var jf = require('jsonfile');
+//var util = require('util');
+var fs = require('fs');
 
 //As first step we need to use the function 'connect' of the object 'linino.Board()'' 
 board.connect( function(){
@@ -56,10 +60,12 @@ board.connect( function(){
    //of the board
    connection.onopen = function (session, details) {
 
+      restartMeasure(board);
+
       //DEBUG
       //console.log("SESSION ID"+Object.getOwnPropertyNames(session));
-      //console.log("AAAAAAAAAAAAAAAAAAAaa:::::::::"+session._realm);
-      //console.log("AAAAAAAAAAAAAAAAAAAaa:::::::::"+session._id);
+      console.log("AAAAAAAAAAAAAAAAAAAaa:::::::::"+session._realm);
+      console.log("AAAAAAAAAAAAAAAAAAAaa:::::::::"+session._id);
 
       //Define a RPC to Read Data from PIN
       function readDigital(args){
@@ -108,19 +114,40 @@ board.connect( function(){
       }
 
       //Define a RPC to Set a Misure
-      function setMisure(ars){
-         try{
-            //do something
+      function sendTemp(args){
+         var pin           = args[0];
+         var operation     = args[1];
+         var measureName   = args[2];
+         var measureTime   = args[3];
+         var response = {};
+/*
+         try{/*
+            if(sendTempFlag){
+               response.resource="",
+               response.name="",
+               return response;
+            }
+         
+
+         
+            misura = setInterval(function(){
+               sensor = board.analogRead(pin);
+               volt =  (sensor /1024.0) * 4.54;
+               cel = (volt - 0.5) * 100;
+               console.log("Measure name::"+measureName+" value::"+cel);
+
+            },measureTime);
+            return 0;
          }catch(ex){
             return ex.message;
-         }
+         }*/
       }
 
 
 
       //Register a RPC for remoting
       //session.register(os.hostname()+'command.rpc.setMisure', setMisure);
-      session.register(board_code+'.command.rpc.setMisure', setMisure);
+      session.register(board_code+'.command.rpc.temp', sendTemp);
       //session.register(os.hostname()+'.command.rpc.setmode', setMode);
       session.register(board_code+'.command.rpc.setmode', setMode);
       
@@ -308,6 +335,108 @@ board.connect( function(){
 
 
 });
+
+//Function to check and restart measure
+function restartMeasure(m_board){
+   var obj           = JSON.parse(fs.readFileSync('setting.json'));
+   //console.log(obj);
+   //console.log("OBJ");
+   //console.dir(obj);
+   
+   var time        = obj.config.board.sensors.temp.time;
+   var resourceid  = obj.config.board.sensors.temp.resource_id;
+   var authid      = obj.config.board.sensors.temp.auth_id;
+
+   
+   
+   if(obj.config.board.sensors.temp.status == 'on'){
+      console.log("Measure Temp is Started!!");
+      startTempMeasure(m_board,time,resourceid,authid);
+   }
+   else{
+      //DEBUG
+      console.log("Measure Temp is Stopped!!");
+   }
+}
+
+function startTempMeasure(m_board, m_time, m_resourceid, m_authid){
+   var pin           = 'A4';
+   //try{
+      misuraTemp = setInterval(function(){
+         var record = [];
+         var sensor = m_board.analogRead(pin);
+         var volt =  (sensor / 1024.0) * 4.54;
+         var cel = (volt - 0.5) * 100;
+
+         console.log(record+","+sensor+","+volt+","+cel);
+
+         var header = {
+            'Content-Type': "application/json", 
+            'Authorization' : m_authid
+         };
+
+         var options = {
+            host: 'smartme-data.unime.it',
+            port: 80,
+            path: '/api/3/action/datastore_upsert',
+            method: 'POST',
+            headers: header
+         };
+
+         console.log(options);
+   
+         record.push({
+            Date: new Date().toISOString(),
+            Temperature: cel,
+            Altitude: 0,
+            Latitude: 38.259525, 
+            Longitude :15.595515
+         });
+
+         var payload = {
+            resource_id : m_resourceid, 
+            method: 'insert', 
+            records : record
+         }
+
+         var payloadJSON = JSON.stringify(payload);
+
+         //console.log(options);
+         //console.dir(payloadJSON);
+
+         
+         var req = http.request(options, function(res) {
+            res.setEncoding('utf-8');
+
+            var responseString = '';
+
+            res.on('data', function(data) {
+               responseString += data;
+               //console.log('On Data: '+ responseString);
+            });
+
+            res.on('end', function() {
+               var resultObject = JSON.parse(responseString);
+               console.log('On End: ');
+               console.dir(resultObject);
+            });
+         });
+
+         req.on('error', function(e) {
+            console.log('On Error:'+e);
+  
+         });
+
+         req.write(payloadJSON);
+
+         req.end();
+
+      },m_time);
+      //return 0;
+      //}catch(ex){
+      //   return ex.message;
+      //}*/
+}
 
 /*
 This function export a generic local service of the board 
