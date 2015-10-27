1367433d93d09f582a9b1a364d779d16f36ee937
Nicola Peditto <n.peditto@gmail.com>
2015-10-27 10:24:17 +0100

    updated lux_plugin with forward of the measures to CKAN

--- plugins.examples/lux_plugin.js
+++ plugins.examples/lux_plugin.js
@@ -2,7 +2,18 @@ exports.main = function (arguments){
     
     var pin = arguments.pin;
     var timer = arguments.timer;
+
+    var m_authid = arguments.m_authid;
+    var m_resourceid = arguments.m_resourceid;
     
+    var http = require('http');
+
+    nconf = require('nconf');
+    nconf.file ({file: 'settings.json'});
+
+    var altitude = nconf.get('config:board:position:altitude');
+    var longitude = nconf.get('config:board:position:longitude');
+    var latitude = nconf.get('config:board:position:latitude'); 
     
     var linino = require('ideino-linino-lib');
     var board = new linino.Board();
@@ -10,10 +21,72 @@ exports.main = function (arguments){
     
     board.connect(function() {
         
-    setInterval(function(){
-        var voltage = m_board.analogRead(pin);
+      setInterval(function(){
+      
+	var record = [];
+	    
+        var voltage = board.analogRead(pin);
         var ldr = (2500/(5-voltage*0.004887)-500)/3.3;
-        console.log("Luminosity: " + ldr);
-      },timer);
+        
+	var header = {
+	  'Content-Type': "application/json", 
+	  'Authorization' : m_authid
+	};	
+	
+	var options = {
+	    host: 'smartme-data.unime.it',
+	    port: 80,
+	    path: '/api/3/action/datastore_upsert',
+	    method: 'POST',
+	    headers: header
+	};	
+	
+	record.push({
+	    Date: new Date().toISOString(),
+	    Brightness: ldr,
+	    Altitude: altitude,
+	    Latitude: latitude, 
+	    Longitude: longitude  
+	});
 
+	var payload = {
+	    resource_id : m_resourceid, 
+	    method: 'insert', 
+	    records : record
+	};
+	
+	var payloadJSON = JSON.stringify(payload);
+	
+	var req = http.request(options, function(res) {
+	  
+	    res.setEncoding('utf-8');
+
+	    var responseString = '';
+
+	    res.on('data', function(data) {
+
+	    });
+
+	    res.on('end', function() {
+
+	    });
+	    
+	});
+
+	req.on('error', function(e) {
+	    console.log('On Error:' + e);
+	});
+
+	req.write(payloadJSON);
+
+	req.end();
+      
+	console.log("PAYLOAD:\n" + payloadJSON);	
+	console.log("Brightness: " + ldr + " (lux) sent to CKAN");
+
+	
+      },timer);
+      
+    });
+    
 }
