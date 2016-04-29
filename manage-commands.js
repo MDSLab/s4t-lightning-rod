5b8a0d89f0b90272aa2e7d2c2126bcbe784ab382
Nicola Peditto <n.peditto@gmail.com>
2016-04-29 18:34:33 +0200

    Added credits.

--- manage-commands.js
+++ manage-commands.js
@@ -1,3 +1,11 @@
+/*
+*				 Apache License
+*                           Version 2.0, January 2004
+*                        http://www.apache.org/licenses/
+*
+*      Copyright (c) 2014 2015 2016 Dario Bruneo, Francesco Longo, Giovanni Merlino, Andrea Rocco Lotronto, Arthur Warnier, Nicola Peditto
+* 
+*/
 
 //service logging configuration: "manageCommands"   
 var logger = log4js.getLogger('manageCommands');
