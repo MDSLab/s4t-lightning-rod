c100433faddad71cd4fdd2eafc8b54aeecfc5002
Nicola Peditto <n.peditto@gmail.com>
2016-04-29 18:38:25 +0200

    Added credits.

--- lightning-rod.js
+++ lightning-rod.js
@@ -1,12 +1,12 @@
 /*
- * 				  Apache License
- *                           Version 2.0, January 2004
- *                        http://www.apache.org/licenses/
- * 
- * Copyright (c) 2014 2015 2016 Dario Bruneo, Francesco Longo, Giovanni Merlino, Andrea Rocco Lotronto, Arthur Warnier, Nicola Peditto
- * 
- * 
- */
+*				 Apache License
+*                           Version 2.0, January 2004
+*                        http://www.apache.org/licenses/
+*
+*      Copyright (c) 2014 2015 2016 Dario Bruneo, Francesco Longo, Giovanni Merlino, Andrea Rocco Lotronto, Arthur Warnier, Nicola Peditto
+* 
+*/
+
 
 //Loading configuration file
 nconf = require('nconf');
--- network-wrapper.js
+++ network-wrapper.js
@@ -1,3 +1,13 @@
+/*
+*				 Apache License
+*                           Version 2.0, January 2004
+*                        http://www.apache.org/licenses/
+*
+*      Copyright (c) 2015 2016 Dario Bruneo, Francesco Longo, Giovanni Merlino, Fabio Verboso, Nicola Peditto
+* 
+*/
+
+
 
 nconf = require('nconf');
 nconf.file ({file: 'settings.json'});
